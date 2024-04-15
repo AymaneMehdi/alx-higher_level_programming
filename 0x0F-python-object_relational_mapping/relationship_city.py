4ecb58a87be041404bd80a4118cd3c02eb8f839d
Aymane Mehdi <unlocked.dark.web@gmail.com>
2024-04-15 03:22:47 +0100

    Update relationship_city.py

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -11,4 +11,4 @@ class City(Base):
     id = Column(Integer, nullable=False, primary_key=True,
                 autoincrement=True, unique=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
+    state_id = Column(Integer, ForeignKey(states.id), nullable=False)
