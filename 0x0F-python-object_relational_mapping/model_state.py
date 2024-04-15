ccc2207629e0a95fd53cd383e6c7679aa683efa6
Aymane Mehdi <unlocked.dark.web@gmail.com>
2024-04-15 03:24:45 +0100

    Update model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -12,6 +12,8 @@ Base = declarative_base()
 class State(Base):
     """Class representing the states table"""
     __tablename__ = 'states'
+
     id = Column(Integer, primary_key=True, nullable=False,
                 autoincrement=True, unique=True)
     name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state")
